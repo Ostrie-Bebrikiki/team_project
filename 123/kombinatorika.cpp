7a2795af435edc0865a94ae96a00e4d0f74a5c47
KrupenyaAleksandr <krupenya21as@gmail.com>
2022-03-19 19:01:56 +0300

    update

--- 123/kombinatorika.cpp
+++ 123/kombinatorika.cpp
@@ -13,7 +13,8 @@ void menu_kombinatorika()
 	char cmd;
 
 	printf_s("1 - Размещение с повторениями\n2 - Размещенеие без повторений\n3 - Сочетание с повторением\n4 - Сочетание без повторений\n5 - Перестановки\n0 - Вернуться\n");
-	scanf_s("%c", &cmd);
+	cmd = _getche();
+
 	switch(cmd)
 	{
 	case '1': placement_with(); break;
@@ -28,6 +29,46 @@ void menu_kombinatorika()
 void placement_with()
 {
 	system("cls");
+	int n, k, x;
+	char cmd;
+	while (true)
+	{
+		printf_s("Введите количество символов n: ");
+		do
+		{
+			scanf_s("%d", &n);
+			if (n < 1)
+			{
+				printf_s("Неверный ввод.\n\n? : ");
+			}
+		} while (n < 1);
+		x = n;
+		printf_s("Введите количество позиций k: ");
+		do
+		{
+			scanf_s("%d", &k);
+			if (k < 1)
+			{
+				printf_s("Неверный ввод.\n\n? : ");
+			}
+		} while (k < 1);
+
+		for (int i = 1; i < k; i++)
+		{
+			x *= n;
+		}
+
+		printf_s("\nОтвет : %d\n", x);
+
+		printf_s("1 - Ещё раз.\n2 - В меню.\n\n? : ");
+
+		cmd = _getche();
+		if (cmd == '2')
+		{
+			break;
+			system("cls");
+		}
+	}
 }
 
 void placement_without()
