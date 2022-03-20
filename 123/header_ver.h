002478e00457bb85461ded863ecf6ece6b1697b9
prorok12672332 <59365875+prorok12672332@users.noreply.github.com>
2022-03-20 12:29:06 +0300

    1

--- 123/header_ver.h
+++ 123/header_ver.h
@@ -6,6 +6,38 @@
 #include <string>
 using namespace std;
 
+
+void input_d(double& a, string str) {
+	while (true) {
+		std::cout << str << ": ";
+		std::cin >> a;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			std::cout << "Ошибка ввода" << std::endl;
+		}
+		else {
+			break;
+		}
+	}
+}
+void input(int& a, string str) {
+	while (true) {
+		std::cout << str << ": ";
+		std::cin >> a;
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(32767, '\n');
+			std::cout << "Ошибка ввода" << std::endl;
+		}
+		else {
+			break;
+		}
+	}
+}
+
 void ver();
 void disp();
 void MathAwait();
