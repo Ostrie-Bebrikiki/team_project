6ac3c142eca0725ba9522ebce315fd87dd76c5d8
prorok12672332 <59365875+prorok12672332@users.noreply.github.com>
2022-03-20 22:11:41 +0300

    Я твой мама в кино водиль

--- 123/Fucktions.cpp
+++ 123/Fucktions.cpp
@@ -1,27 +1,5 @@
 #include "Functions.h"
 
-double f(char ch, double x, double a, double b, double c, double d) {
-	switch (ch) {
-	case '2': return a * pow(x, b) + c;
-	case '3': return a * pow(b, c * x) + d;
-	case '4': return a * log(b * x) + c;
-	case '5': return a * sin(b * x + c) + d;
-	case '6': return a * cos(b * x + c) + d;
-	default: return 0;
-	}
-}
-void choose(char &s) {
-	while (true) {
-		std::cout << "Выберите функцию" << endl << "1 - Полином степени N: a0+a1**x+a2*x^2+...+aN*x^N" << endl
-			<< "2 - Степенная: a*x^b+c" << endl << "3 - Показательная: a*b^(c*x)+d" << endl << "4 - Логарифмическая: a*ln(b*x)+c" << endl
-			<< "5 - Синусоида: a*sin(b*x+c)+d" << endl << "6 - Косинусоида: a*cos(b*x+c)+d" <<endl<<"0 - Назад" << endl << '>';
-		s = _getche();
-		if (s == '0' || s == '1' || s == '2' || s == '3' || s == '4' || s == '5' || s == '6') break;
-	}
-}
-
-
-
 void menu_Func() {
 	char s, ch;
 	
@@ -68,8 +46,8 @@ void integral(char ch, double a, double b, double c, double d) {
 		input_d(D2, "Введите правую границу");
 		if (D1 == 0.0f && D2 == 0.0f) return;
 		if (D2 < D1) cout << "Левая граница не может быть больше правой" << endl;
-		else if(ch == '2' && D1 < 0 && double((int)b) != b) cout << "Нельзя найти интеграл при границе меньше нуля и нецелой степенью\nДля выхода введите 0 0\n";
-		else if(ch == '3' && b < 0 && (double((int)c*D1) != c*D1 || double((int)c * D2) != c * D2)) cout << "Нельзя найти интеграл при b < 0 и нечетном c*x\nДля выхода введите 0 0\n";
+		else if(ch == '2' && D1 < 0 && double((int)b) != b) cout << "Нельзя найти интеграл при основании меньше нуля и нецелой степенью\nДля выхода введите 0 0\n";
+		else if(ch == '3' && b < 0 && (double((int)c*D1) != c*D1 || double((int)c * D2) != c * D2)) cout << "Нельзя найти интеграл при основании меньше нуля и нецелой степенью\nДля выхода введите 0 0\n";
 		else if(ch == '4' && D1 <= 0) cout << "Нельзя найти интеграл при границе меньше либо равной нулю\nДля выхода введите 0 0\n";
 		else break;
 	}
@@ -116,7 +94,52 @@ void integral(char ch, double a, double b, double c, double d) {
 	}
 }
 void graph(char ch, double a, double b, double c, double d) {
+	const int WIDTH = 800, HEIGHT = 600;
+	const int width = 49 * WIDTH / 50, height = 49 * HEIGHT / 50;
+	int i1, i2;
+	SDL_Event e;
+	bool quit = 0;
+	int D1, D2;
+	while (true) {
+		input(D1, "Введите левую границу");
+		input(D2, "Введите правую границу");
+		if (D1 == 0.0f && D2 == 0.0f) return;
+		if (D2 < D1) cout << "Левая граница не может быть больше правой" << endl;
+		else break;
+	}
+	int h = width / (abs(D1) > abs(D2)? abs(2 * D1): abs(2 * D2));
 
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow(u8"Леееееее",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+	SDL_SetRenderDrawColor(renderer, 128, 128, 128, 0);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+	SDL_RenderDrawLine(renderer, WIDTH / 2, HEIGHT / 100, WIDTH / 2, 99 * HEIGHT / 100);
+	for (i1 = WIDTH / 2 + h; i1 <= 99 * WIDTH / 100; i1 += h) {
+		
+		SDL_RenderDrawLine(renderer, i1, HEIGHT / 2 - HEIGHT / 100, i1, HEIGHT / 2 + HEIGHT / 100);
+	}
+	for (i2 = WIDTH / 2 - h; i2 >= WIDTH / 100; i2 -= h) {
+		
+		SDL_RenderDrawLine(renderer, i2, HEIGHT / 2 - HEIGHT / 100, i2, HEIGHT / 2 + HEIGHT / 100);
+	}
+	i2 += h, i1 -= h;
+	SDL_RenderDrawLine(renderer, i2, HEIGHT / 2, i1, HEIGHT / 2);
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+			}
+		}
+		SDL_RenderPresent(renderer);
+		SDL_Delay(200);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 void root_f(char ch, double a, double b, double c, double d) {
 
--- 123/Functions.h
+++ 123/Functions.h
@@ -39,6 +39,61 @@ void input(int& a, std::string str) {
 		}
 	}
 }
+void DrawCircle(SDL_Renderer* renderer, int32_t centreX, int32_t centreY, int32_t radius)
+{
+	const int32_t diameter = (radius * 2);
+
+	int32_t x = (radius - 1);
+	int32_t y = 0;
+	int32_t tx = 1;
+	int32_t ty = 1;
+	int32_t error = (tx - diameter);
+
+	while (x >= y)
+	{
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX + x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY - y);
+		SDL_RenderDrawPoint(renderer, centreX - x, centreY + y);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX + y, centreY + x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY - x);
+		SDL_RenderDrawPoint(renderer, centreX - y, centreY + x);
+
+		if (error <= 0)
+		{
+			++y;
+			error += ty;
+			ty += 2;
+		}
+
+		if (error > 0)
+		{
+			--x;
+			tx += 2;
+			error += (tx - diameter);
+		}
+	}
+}
+double f(char ch, double x, double a, double b, double c, double d) {
+	switch (ch) {
+	case '2': return a * pow(x, b) + c;
+	case '3': return a * pow(b, c * x) + d;
+	case '4': return a * log(b * x) + c;
+	case '5': return a * sin(b * x + c) + d;
+	case '6': return a * cos(b * x + c) + d;
+	default: return 0;
+	}
+}
+void choose(char& s) {
+	while (true) {
+		std::cout << "Выберите функцию" << endl << "1 - Полином степени N: a0+a1**x+a2*x^2+...+aN*x^N" << endl
+			<< "2 - Степенная: a*x^b+c" << endl << "3 - Показательная: a*b^(c*x)+d" << endl << "4 - Логарифмическая: a*ln(b*x)+c" << endl
+			<< "5 - Синусоида: a*sin(b*x+c)+d" << endl << "6 - Косинусоида: a*cos(b*x+c)+d" << endl << "0 - Назад" << endl << '>';
+		s = _getche();
+		if (s == '0' || s == '1' || s == '2' || s == '3' || s == '4' || s == '5' || s == '6') break;
+	}
+}
 
 void integral(char ch, double a, double b, double c, double d);
 void graph(char ch, double a, double b, double c, double d);
