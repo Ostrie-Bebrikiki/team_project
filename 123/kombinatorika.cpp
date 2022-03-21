08f6620c7063dd7a1490f948fa0f37a123ed7279
KrupenyaAleksandr <krupenya21as@gmail.com>
2022-03-21 20:00:35 +0300

    пофиксить stoi

--- 123/kombinatorika.cpp
+++ 123/kombinatorika.cpp
@@ -468,5 +468,91 @@ void transposition_without()
 
 void transposition_with()
 {
+	system("cls");
+	int i, error, sum = 0;
+	string tmp;
+	unsigned int n, a, fact, tmpF;
+	char cmd;
+	while (true)
+	{
+		unsigned long long int x = 1, fact = 1, tmpF = 1;
+		printf_s("Введите количество n различных элементов, входящих в перестановку: ");
+		do
+		{
+			do
+			{
+				i = 0;
+				error = 0;
+				getline(cin, tmp);
+				while (i < tmp.length())
+				{
+					if (isdigit(tmp[i]) == 0)
+					{
+						printf_s("Неверный ввод.\n\n? : ");
+						error = 1;
+						break;
+					}
+					i++;
+				}
+			} while (error == 1);
 
+			n = stoi(tmp, 0, 10);
+
+			if (n < 2)
+			{
+				printf_s("Некорректный ввод.\n\n? : ");
+			}
+		} while (n < 2);
+
+		for (int j = 1; j <= n; j++)
+		{
+			printf_s("Введите количество элементов в множестве n%d: ", j);
+			do
+			{
+				i = 0;
+				error = 0;
+				getline(cin, tmp);
+				while (i < tmp.length())
+				{
+					if (isdigit(tmp[i]) == 0)
+					{
+						printf_s("Неверный ввод.\n\n? : ");
+						error = 1;
+						break;
+					}
+					i++;
+				}
+			} while (error == 1);
+
+			a = stoi(tmp, 0, 10);
+
+			tmpF = 1;
+			sum += a;
+			for (int l = 1; l <= a; l++)
+			{
+				tmpF *= l;
+			}
+			
+			fact *= tmpF;
+		}
+
+		tmpF = 1;
+		for (int h = 1; h <= sum; h++)
+		{
+			tmpF *= h;
+		}
+
+		x = tmpF / fact;
+		printf_s("\nОтвет : %lu\n", x);
+
+		printf_s("\n1 - Вернуться в меню.\nЧтобы использовать калькулятор ещё раз, нажмите любую кнопку.\n\n? : ");
+		cmd = _getche();
+		if (cmd == '1')
+		{
+			system("cls");
+			return;
+		}
+
+		system("cls");
+	}
 }
